304a8961f37f1450cf4fd4b7f7b3d5e0176e8f64
Edil <usobaiev1@gmail.com>
2023-10-07 19:18:16 +0600

    Load plananets data in test

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -10,14 +10,17 @@ const {
     mongoDisconnect
 } = require("../../services/mongo")
 
+const { loadPlanetsData } = require("../../models/planets.model")
+
 describe("Launches API", () => {
     beforeAll( async() => {
         await mongoConnect()
+        await loadPlanetsData()
     })
 
-    // afterAll(async () => {
-    //     await mongoDisconnect()
-    // })
+    afterAll(async () => {
+        await mongoDisconnect()
+    })
     describe("Test GET /launches", () => {
         test("It should respond with 200 success", async () => { 
             const response = await request(app)
